08993e0aa2d47e175ca48fbf86090ed0fabbf298
Martina Baez <martinabaez.cc@gmail.com>
2022-09-09 16:02:07 -0300

    correccion

--- src/App.js
+++ src/App.js
@@ -1,28 +1,10 @@
+import navBar from './components/navbar'
 
 function App() {
   return (
-    <nav className="navbar navbar-expand-lg bg-dark bg-gradient">
-  <div className="container-fluid">
-    <a className="navbar-brand text-white" href="#">MERAKI Store</a>
-    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-      <span className="navbar-toggler-icon"></span>
-    </button>
-    <div className="collapse navbar-collapse" id="navbarNav">
-      <ul className="navbar-nav">
-        <li className="nav-item">
-          <a className="nav-link active text-white-50" aria-current="page" href="#">Home</a>
-        </li>
-        <li className="nav-item">
-          <a className="nav-link text-white-50" href="#">Products</a>
-        </li>
-        <li className="nav-item">
-          <a className="nav-link text-white-50" href="#">Contact</a>
-        </li>
-      </ul>
-    </div>
-  </div>
-</nav>
+    <navBar/>
   );
 }
 
 export default App;
+
